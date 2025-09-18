e1411b0f38d3b1637b041a04625622daa5c365c5
Andrew Smith <andrew.c.smith03.com>
2025-09-18 17:24:27 -0400

    added most of the model

--- game_review_platform/feed/models.py
+++ game_review_platform/feed/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    game = models.CharField(max_length=100)
+    cover_art = models.ImageField()
+    playtime = models.DecimalField(..., max_digits=6, decimal_places=2)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
