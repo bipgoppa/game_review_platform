f7e08cf6f08a8b0b74f935757f6be7506ae9ec1c
Andrew Smith <andrew.c.smith03.com>
2025-09-23 16:01:54 -0400

    Setting up the model for a single review, and then populating all reviews to feed

--- game_review_platform/IGDReviews/models.py
+++ game_review_platform/IGDReviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    game = models.CharField(max_length=100)
+    cover_art = models.ImageField()
+    playtime = models.DecimalField(max_digits=6, decimal_places=2)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- game_review_platform/feed/views.py
+++ game_review_platform/feed/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from IGDReviews.models import Review
 
+# Create your views here.
 def home(request):
-    return render(request, 'feed/home.html')
+    myReviews = Review.objects.all().values()
+    template = loader.get_template('feed/home.html')
+    context = {'myReviews': myReviews,}
+    return HttpResponse(template.render(context, request))
