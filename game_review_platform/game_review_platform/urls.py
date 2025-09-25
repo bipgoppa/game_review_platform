7bdbb804068de6b2f6e2a488a808c1b043158f70
Joshua White <joshuawhite706@gmail.com>
2025-09-25 16:46:51 -0400

    added boxes that will end up displaying what we want from our reviews database

--- game_review_platform/game_review_platform/urls.py
+++ game_review_platform/game_review_platform/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
-    path('', include('feed.urls')),
+    path('feed/', include('feed.urls')),
 
 ]
