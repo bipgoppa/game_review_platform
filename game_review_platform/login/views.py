2140f4ad333bc1a44673b2f4318c1a2b9eb34519
Zoe Thornsbury <zthornsbury@mocs.flsouthern.edu>
2025-09-25 17:08:18 -0400

    Link login app to database and add login view

--- game_review_platform/login/views.py
+++ game_review_platform/login/views.py
@@ -2,19 +2,25 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponse
+from .forms import LoginForm
 
 
 def user_login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            messages.error(request, 'Invalid username or password')
-    return render(request, 'login/login.html')
+            return render(request, 'login/login.html')
+
+    else:
+        form = LoginForm()
+    return render(request, 'login/login.html', {'form': form})
 
 
 def user_logout(request):
